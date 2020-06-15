32af536730d869d6063da928631f6da9b1f4200e
David Kramer <davidkramer@me.com>
2020-06-14 22:53:35 -0700

    Initial project commit

---
+++ ConcernedEmu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConcernedEmu
+//
+//  Created by David Kramer on 6/13/20.
+//  Copyright © 2020 David Kramer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
